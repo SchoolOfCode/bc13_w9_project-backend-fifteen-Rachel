4905907c4e8e9e9601af42fd045cc9151d90effe
Rachel Morris <rachelmorris262@outlook.com>
2022-12-05 22:17:33 +0000

    refactored models

--- db/index.js
+++ db/index.js
@@ -1,20 +1,12 @@
-// const { Pool } = require("pg");
-// const pool = new Pool({
-//   connectionString: process.env.POSTGRES_CONNECTION_URL,
-// });
-
-// module.exports = {
-//   query: function (text, params) {
-//     return pool.query(text, params);
-//   },
-// };
-
+// This file imports the pg module which allows node.js to connect with the postgreSQL database
 import pg from "pg";
 
+// This is where node.js is connected to the postgreSQL with the url link kept in .env file
 export const pool = new pg.Pool({
     connectionString: process.env.POSTGRES_CONNECTION_URL,
 });
 
+// This function allows queries that are params to be passed to the postgreSQL without the risk of string concatenation
 export function query(text, params) {
     return pool.query(text, params);
 }
--- models/notes.js
+++ models/notes.js
@@ -1,25 +1,17 @@
-import { pool } from "../db/index.js"; 
+import { query } from "../db/index.js"; 
 
 export async function getNotes() {
-    const result = await pool.query("SELECT * FROM notes;")
-    const notesArray = result.rows;
-    return notesArray.map(note => ({
-        ...note,
-        time: parseInt(note.time)
-    }));
+    const result = await query("SELECT * FROM notes;")
+    return result.rows;  
 }
 
 export async function addNote(newContent) {
-    const sqlQuery = "INSERT INTO notes (content) VALUES ($1)  RETURNING *;";
-    const param = [newContent.content];
-    const result = await pool.query(sqlQuery, param);
-    const created = result.rows[0]
-    return created
+    const result = await query("INSERT INTO notes (content) VALUES ($1)  RETURNING *;", [newContent.content]);
+    return result.rows[0]
 }   
 
 export async function deleteNote(deleteId) {
-    const result = await pool.query("DELETE FROM notes WHERE id = ($1) RETURNING*;", [deleteId]);
-    const deleted = result.rows[0];
-    return deleted
+    const result = await query("DELETE FROM notes WHERE id = ($1) RETURNING*;", [deleteId]);
+    return result.rows[0];
 }
 
