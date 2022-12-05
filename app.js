08258749d41c62b6f66045fdacbef267ad8e3195
Rachel Morris <rachelmorris262@outlook.com>
2022-12-05 16:47:54 +0000

    working

--- app.js
+++ app.js
@@ -2,26 +2,20 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require('cors')
 const app = express();
-const PORT = process.env.port || 3002;
+const PORT = process.env.port || 3001;
 
 
 //test
 
 const noteRouter = require("./routes/notes.js");
 
-const corsOptions = {
-  origin: 'http://localhost:3001',
-  optionsSuccessStatus: 200
-};
 
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(morgan("dev"));
 app.use(express.static("public"));
 app.use(express.json());
 
-app.use("/api/movies", movieRouter);
-app.use("/api/directors", directorRouter);
 app.use("/api/notes", noteRouter);
 
 app.listen(PORT, function () {
